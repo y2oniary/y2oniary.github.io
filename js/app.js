51c05e285ca3462c6f4a0a7a466e1ace504f245d
chayeoi <algumuni1@gmail.com>
2017-12-18 10:36:26 +0900

    seperate js

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    works: [
+      {
+        name: "Static Layout",
+        link: "./static-layout/index.html"
+      },
+      {
+        name: "Grid Layout",
+        link: "./grid-layout/index.html"
+      },
+      {
+        name: "Snake Game",
+        link: "https://chayeoisnakegame.netlify.com/"
+      }
+    ]
+  }
+});
